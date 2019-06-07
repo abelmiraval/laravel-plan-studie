7f9adece777032bee24bc7d093315c43907d5f18
AbelSaul <abelmiraval@gmail.com>
2019-06-07 05:23:36 -0500

    Add forceSchema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Mixed content
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
